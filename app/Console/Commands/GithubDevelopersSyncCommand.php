e70d014a7137228da6cb6cdb156380743c192d48
Mario Tinelli <maariotinelli@gmail.com>
2024-05-29 17:22:49 -0300

    feat: updated readme

--- app/Console/Commands/GithubDevelopersSyncCommand.php
+++ app/Console/Commands/GithubDevelopersSyncCommand.php
@@ -4,17 +4,15 @@ namespace App\Console\Commands;
 
 use App\Jobs\GithubDevelopersSyncJob;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Redis;
 
 class GithubDevelopersSyncCommand extends Command
 {
-    protected $signature = 'github-developers-sync';
+    protected $signature = 'fetch:developers';
 
-    protected $description = 'Sync users from Github API and store in database.';
+    protected $description = 'Fetch developers from Github API and store in database.';
 
     public function handle(): void
     {
-        Redis::command('flushdb');
         GithubDevelopersSyncJob::dispatch();
     }
 }
